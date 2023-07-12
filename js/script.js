0aabebc20650fc213128b4700414308d8dd46096
TonyLig <118136260+TonyLig@users.noreply.github.com>
2023-07-12 18:14:22 +0200

    add date

--- js/script.js
+++ js/script.js
@@ -36,5 +36,49 @@ function displayData(response) {
     const today = new Date();
     const date = document.querySelector(".date");
     date.innerText = dateFunction(today);
+
+    const temp = document.querySelector(".temp");
+    temp.innerHTML = `Temp: ${Math.round(response.main.temp)} <span>°c</span>`;
+
+    const weather = document.querySelector(".weather");
+    weather.innerText = `Weather ${response.weather[0].main}`;
+
+    const tempRange = document.querySelector(".temp-range");
+    tempRange.innerText = `Temp Range: ${Math.round(
+      response.main.temp_min
+    )}°C / ${Math.round(response.main.temp_max)}°C`;
   }
-}
+
+
+function dateFunction(d) {
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day}, ${date} ${month} ${year}`;
+};
